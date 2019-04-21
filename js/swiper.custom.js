8a726df1940f8e0f7e68184346aaf55ac5c97272
Winston <winston.barlow@code.berlin>
2019-04-21 22:58:43 +0200

    adding base website files & dir

---
+++ js/swiper.custom.js
@@ -0,0 +1,28 @@
+/*------------------------------------------------------------------
+Initialize Swiper
+-------------------------------------------------------------------*/
+"use strict";
+var swiper = new Swiper('.testimonials', {
+  speed: 600,
+  slidesPerView: "auto",
+  spaceBetween: 0,
+  pagination: {
+	el: '.swiper-pagination',
+	dynamicBullets: true,
+	clickable : true
+  }
+});
+var swipert = new Swiper ('.hiw-titles', {
+spaceBetween: 1,
+centeredSlides: true,
+slidesPerView: 'auto',
+touchRatio: 1,
+slideToClickedSlide: true
+}); 
+var swiperc = new Swiper ('.hiw-content', {
+direction: 'horizontal',
+effect: 'slide'
+}); 
+
+swipert.controller.control = swiperc;
+swiperc.controller.control = swipert;
